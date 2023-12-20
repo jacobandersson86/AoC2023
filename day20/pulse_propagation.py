47e5f88fd654c4727ccc4e73e9422261c74e5a73
Jacob Andersson <jacob.andersson@plejd.com>
2023-12-20 21:48:35 +0100

    solve day20 part 1

--- day20/pulse_propagation.py
+++ day20/pulse_propagation.py
@@ -1,39 +1,4 @@
-
-def process(module, rack, inputs, outputs) :
-    operator, name, out = rack[module]
-
-    # Check states
-    input_names =  inputs[name]
-    if len(input_names) != 0 :
-        input_states, last_input_state = zip(*[outputs[name] for name in input_names])
-    else:
-        input_states, last_input_state = [], []
-    output_state, _ = outputs[name]
-
-    # Check operation
-
-    process_output = 0
-    match operator :
-        case '&' :
-            if all([state == 1 for state in last_input_state]) :
-                process_output = 1
-        case '%' :
-            if all([state == 0 for state in input_states]):
-                process_output = (output_state + 1) % 2
-        case None :
-            if len(input_names) != 0: # Broadcaster
-                process_output = input_states[0]
-            else : # Button
-                process_output = 1
-    # Update outputs
-    outputs[name] = (process_output, output_state)
-
-    # A pulse is when we changed state
-    if process_output == output_state :
-        out = []
-
-    return out, process_output
-
+import re
 def initiate_inputs(rack, outputs) :
     inputs = dict()
     for module in rack :
@@ -52,13 +17,11 @@ def initiate_outputs(rack) :
 
     for module in rack :
         _, name, _ = rack[module]
-        outputs[name] = (0, 0)
+        outputs[name] = 0
 
     return outputs
 
-
 example1 = {
-    'button' :  (None, "button", ["broadcaster"]),
     'broadcaster' : (None, 'broadcaster', ['a', 'b', 'c']),
     'a' :       ('%', 'a', ['b']),
     'b' :       ('%', 'b', ['c']),
@@ -66,52 +29,103 @@ example1 = {
     'inv':      ('&', 'inv', ['a']),
 }
 
-def process_loop(module_queue, rack, inputs, outputs, count) :
-    slots = module_queue.pop(0)
-    for module in slots :
-        next_modules, pulse = process(module, rack, inputs, outputs)
-        module_queue.append(next_modules)
+example2 = {
+    'broadcaster'   : (None, 'broadcaster', ['a']),
+    'a'             : ('%', 'a', ['inv', 'con']),
+    'inv'           : ('&', 'inv', ['b']),
+    'b'             : ('%', 'b', ['con']),
+    'con'           : ('&', 'con', ['output']),
+    'output'        : (None, 'output', [])
+}
 
-        high, low = count
-        if pulse :
-            high += 1
+def process_pulse(pulse, to, rack, inputs, outputs):
+    new_pulse = []
+
+    if to not in rack:
+        return []
+
+    operator, _, connections = rack[to]
+
+    new_pulse = False
+
+    match operator :
+        case '&' :
+            new_pulse = True
+                # Check states
+            input_names =  inputs[to]
+            if len(input_names) != 0 :
+                input_states = [outputs[name] for name in input_names]
+            if all([state == 1 for state in input_states]) :
+                outputs[to] = 0
+            else :
+                outputs[to] = 1
+        case '%' :
+            if pulse == 0 :
+                outputs[to] = (outputs[to] + 1) % 2
+                new_pulse = True
+        case None :
+            outputs[to] = pulse
+            new_pulse = True
+
+    next_pulses = []
+    if new_pulse :
+        for connection in connections:
+            next_pulses.append((outputs[to], connection))
+
+    return next_pulses
+
+def read_data(file) :
+    with open(file) as f :
+        lines = f.readlines()
+
+    pattern = re.compile(r'(%|&)(\w+)')
+
+    rack = dict()
+    for line in lines :
+        gate, outputs = line.strip('\n').split(' -> ')
+
+        outputs = re.findall('[a-z]+', outputs)
+
+        if gate == 'broadcaster' :
+            op = None
+            name = 'broadcaster'
         else :
-            low += 1
+            match = pattern.match(gate)
+            op, name = match.groups()
 
-    return high, low
+        rack[name] = (op, name, outputs)
 
+    return rack
 
 def main():
     n_high_pulses = 0
     n_low_pulse = 0
 
-    rack = example1
-
-    module_queue = [["button"]]
+    # rack = example2
+    rack = read_data('day20/input')
 
     outputs = initiate_outputs(rack)
     inputs = initiate_inputs(rack, outputs)
-    outputs['button'] = (1, 0)
 
-    # pulse = process_loop(module_queue, rack, inputs, outputs)
+    n_push = 1000
+    for _ in range(n_push) :
+        pulse_queue = [(0, 'broadcaster')]
+        # The push of the button doesn't count.
 
-    # if pulse :
-    #     n_high_pulses += 1
-    # else :
-    #     n_low_pulse += 1
+        while len(pulse_queue) :
+            pulse = pulse_queue.pop(0)
+            state, reciptent = pulse
 
-    count = (n_high_pulses, n_low_pulse)
+            if state :
+                n_high_pulses += 1
+            else :
+                n_low_pulse += 1
 
-    for _ in range(12) :
-        count = process_loop(module_queue, rack, inputs, outputs, count)
+            new_pulses = process_pulse(state, reciptent, rack, inputs, outputs)
+            pulse_queue.extend(new_pulses)
 
-        if all([outputs[name][0] == 0 for name in outputs]) :
-            # Initial state!
-            break
-
-    n_high_pulses, n_low_pulse = count
     print(f"High {n_high_pulses} Low {n_low_pulse}")
-
+    print(f"Part 1 : {n_high_pulses * n_low_pulse}")
 
 if __name__ == '__main__' :
     main()
