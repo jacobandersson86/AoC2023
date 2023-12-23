aeb7464f5c93675b67e640f763dddc456a5d0175
Jacob Andersson <jacob.andersson@plejd.com>
2023-12-23 19:17:18 +0100

    solve day20 part 2

--- day20/pulse_propagation.py
+++ day20/pulse_propagation.py
@@ -1,4 +1,6 @@
 import re
+import math
+
 def initiate_inputs(rack, outputs) :
     inputs = dict()
     for module in rack :
@@ -21,23 +23,6 @@ def initiate_outputs(rack) :
 
     return outputs
 
-example1 = {
-    'broadcaster' : (None, 'broadcaster', ['a', 'b', 'c']),
-    'a' :       ('%', 'a', ['b']),
-    'b' :       ('%', 'b', ['c']),
-    'c' :       ('%', 'c', ['inv']),
-    'inv':      ('&', 'inv', ['a']),
-}
-
-example2 = {
-    'broadcaster'   : (None, 'broadcaster', ['a']),
-    'a'             : ('%', 'a', ['inv', 'con']),
-    'inv'           : ('&', 'inv', ['b']),
-    'b'             : ('%', 'b', ['con']),
-    'con'           : ('&', 'con', ['output']),
-    'output'        : (None, 'output', [])
-}
-
 def process_pulse(pulse, to, rack, inputs, outputs):
     new_pulse = []
 
@@ -47,14 +32,13 @@ def process_pulse(pulse, to, rack, inputs, outputs):
     operator, _, connections = rack[to]
 
     new_pulse = False
-
     match operator :
         case '&' :
             new_pulse = True
                 # Check states
             input_names =  inputs[to]
             if len(input_names) != 0 :
-                input_states = [outputs[name] for name in input_names]
+                input_states = [outputs[name] for name in input_names if name in outputs]
             if all([state == 1 for state in input_states]) :
                 outputs[to] = 0
             else :
@@ -97,6 +81,70 @@ def read_data(file) :
 
     return rack
 
+def find_flippeliflops(head, rack, inputs) :
+    # Find inputs check attach to head
+    head_inputs = inputs[head]
+
+    # For each input check if is a flip flop
+    found = []
+    next_input = []
+    for input in head_inputs :
+        op ,_ ,_  = rack[input]
+        if op == '%' :
+            found.append(input)
+        else :
+            next_input.append(input)
+
+    # False, go deeper.
+    for input in next_input :
+        found_upstream = find_flippeliflops(input, rack, inputs)
+        found.extend(found_upstream)
+
+    # True return name of flip flop
+    return list(set(found))
+
+def find_connections(head, rack, inputs, depth) :
+    depth -= 1
+
+    head_inputs = inputs[head]
+    found = []
+    if depth > 0 :
+        for input in head_inputs :
+            found_upstream = find_connections(input, rack, inputs, depth)
+            found.extend(found_upstream)
+
+        return list(set(found))
+    else :
+        return head_inputs
+
+
+def find_periods(gates, outputs, periods, n) :
+    all_found = True
+    for gate in gates :
+        last, n_last, period, n_period, first = periods[gate]
+        now = outputs[gate]
+
+        if last == 0 and now == 1:
+            if first == 0 :
+                first = n
+            if period[-1] < 0 :
+                period[-1] += 1
+            else :
+                this_period = n - n_last
+                if period[-1] == this_period:
+                    n_period[-1] += 1
+                else :
+                    period.append(this_period)
+                    n_period.append(1)
+            n_last = n
+
+        last = now
+        periods[gate] = (last, n_last, period, n_period, first)
+
+        if period[-1] <= 0 :
+            all_found = False
+    return periods, all_found
+
 def main():
     n_high_pulses = 0
     n_low_pulse = 0
@@ -110,7 +158,6 @@ def main():
     n_push = 1000
     for _ in range(n_push) :
         pulse_queue = [(0, 'broadcaster')]
-        # The push of the button doesn't count.
 
         while len(pulse_queue) :
             pulse = pulse_queue.pop(0)
@@ -125,7 +172,62 @@ def main():
             pulse_queue.extend(new_pulses)
 
     print(f"High {n_high_pulses} Low {n_low_pulse}")
-    print(f"Part 1 : {n_high_pulses * n_low_pulse}")
+    print(f"Part 1 : {n_high_pulses * n_low_pulse}", end='\n\n')
+
+    # Add rx to outputs (in order to search inputs attached to it)
+    rack['rx'] = (None, 'rx', [])
+
+    # Reset Outputs
+    outputs = initiate_outputs(rack)
+    inputs = initiate_inputs(rack, outputs)
+
+    # Find gates attached to rx
+    heads = find_connections('rx', rack, inputs, 3)
+    print(f"Interesting gates: {heads}")
+
+    rack.pop('rx')
+
+    largest_periods = []
+    for head in heads :
+        print(f"Explore {head}")
+        flip_flops = find_flippeliflops(head, rack, inputs)
+        # Reset Outputs
+        outputs = initiate_outputs(rack)
+        inputs = initiate_inputs(rack, outputs)
+
+        # Find periodicity of those
+        periods = dict()
+        for f in flip_flops :
+            periods[f] = (0, 0, [-3], [0], 0)
+
+        all_found = False
+        n = 0
+        while not all_found :
+            pulse_queue = [(0, 'broadcaster')]
+
+            while len(pulse_queue) :
+                pulse = pulse_queue.pop(0)
+                state, reciptent = pulse
+                new_pulses = process_pulse(state, reciptent, rack, inputs, outputs)
+                pulse_queue.extend(new_pulses)
+
+            periods, all_found = find_periods(flip_flops, outputs, periods, n)
+            n += 1
+
+        for p in periods:
+            print(p, periods[p])
+        n -= 1
+        # Throw LCM at it
+        periods = [(periods[item][2]) for item in periods]
+
+        largest = sorted(periods)[-1][-1]
+        print(f"Largest periods: {largest}")
+        largest_periods.append(largest)
+        print("")
+
+    # Find when all heads are 1 at the same time
+    least = math.lcm(*largest_periods)
+    print(f"Part 2 {least}")
 
 if __name__ == '__main__' :
     main()
